6798083cd57e698b1ff1b3b9e0858adfa8710b12
BhanuKiranChaluvadi <nthdimensionalreality@gmail.com>
2024-01-04 15:51:16 +0000

    added fftw3 package to the CMakeLists.txt inorder to use the fftw3 library for the frequency analysis

---
+++ src/SimpleFrequencyProcessor.cpp
@@ -0,0 +1,85 @@
+#include "SimpleFrequencyProcessor.hpp"
+#include <fftw3.h>
+#include <math.h>
+#include <string_view>
+
+namespace jackmeter {
+SimpleFrequencyProcessor::SimpleFrequencyProcessor(std::string_view name)
+    : m_name(std::string(name))
+    , m_latestPeak(0.0f)
+{
+}
+
+void SimpleFrequencyProcessor::Process(float* samples, uint32_t nSamples)
+{
+    // Perform Fourier transform
+    fftw_complex* out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * nSamples);
+    fftw_plan p = fftw_plan_dft_r2c_1d(nSamples, reinterpret_cast<double*>(samples), out, FFTW_ESTIMATE);
+    fftw_execute(p);
+
+    // Find frequency with highest magnitude
+    /*
+    double maxMagnitude = 0.0;
+    double maxFrequency = 0.0;
+    for (uint32_t i = 0; i < nSamples / 2 + 1; i++) {
+        double magnitude = sqrt(out[i][0] * out[i][0] + out[i][1] * out[i][1]);
+        if (magnitude > maxMagnitude) {
+            maxMagnitude = magnitude;
+            maxFrequency = (double)i * sampleRate / nSamples; // Calculate frequency
+        }
+    }
+
+    // Update min, max, and latest frequency
+    m_latestFrequency = maxFrequency;
+    if (maxFrequency < m_minFrequency) {
+        m_minFrequency = maxFrequency;
+    }
+    if (maxFrequency > m_maxFrequency) {
+        m_maxFrequency = maxFrequency;
+    }
+    */
+    // Clean up
+    fftw_destroy_plan(p);
+    fftw_free(out);
+}
+
+bool SimpleFrequencyProcessor::SignalDetected() const
+{
+    return m_signalDetected;
+}
+
+float SimpleFrequencyProcessor::GetLatestPeak()
+{
+    return m_latestPeak.load();
+}
+
+float SimpleFrequencyProcessor::GetLatestPeakDb()
+{
+    return 20.0f * log10f(m_latestPeak.load());
+}
+
+float SimpleFrequencyProcessor::GetMinPeak()
+{
+    return m_minPeak.load();
+}
+
+float SimpleFrequencyProcessor::GetMinPeakDb()
+{
+    return 20.0f * log10f(m_minPeak.load());
+}
+
+float SimpleFrequencyProcessor::GetMaxPeak()
+{
+    return m_maxPeak.load();
+}
+
+float SimpleFrequencyProcessor::GetMaxPeakDb()
+{
+    return 20.0f * log10f(m_maxPeak.load());
+}
+
+std::string_view SimpleFrequencyProcessor::GetName()
+{
+    return m_name;
+}
+} // namespace jackmeter
